fb936ac2c675e50a0c4dcfe1db00381d9ae2be28
RD Home <ratna.ratna50@gmail.com>
2019-05-16 12:34:06 +0530

    config php added

---
+++ php/user/payment/config - Copy.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_oQynwF06fgPRjt';
+$keySecret = 'kWTgLUexfdnc8Ik59AgczOWG';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
